ef7fa4e15aeeb6e217eae4b57fb599ad2e706e06
NezKap <werras206@mail.ru>
2025-10-02 19:44:34 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "JavaLaba1"
---
+++ src/test/java/org/example/Laba1Test.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Laba1Test {
+    @Test
+    void example() {
+        assertEquals(15.0, 5 + 10);
+    }
+}
